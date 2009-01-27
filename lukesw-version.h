19599557bf71fc1431f2980ee170015ed112650e
ssallinen <ssallinen@4fd11369-b071-49c0-8a2c-c9dcf8991ec9>
2009-01-27 18:28:02 +0000

    0.8.7 release

--- lukesw-version.h
+++ lukesw-version.h
@@ -1,7 +1,7 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 8
-#define VERSION_BUGFIX 6
+#define VERSION_BUGFIX 7
 
-#define STRINGVERSION "0.8.6"
+#define STRINGVERSION "0.8.7"
 
 // rc compiler needs an empty line after this one
