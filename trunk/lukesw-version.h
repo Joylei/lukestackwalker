098632ea21a45b55e3c9069f19955b277cda4e9a
ssallinen <ssallinen@4fd11369-b071-49c0-8a2c-c9dcf8991ec9>
2009-11-19 19:50:34 +0000

    0.9.4 : fix vc runtime dll dependency; add toolbar icons

--- trunk/lukesw-version.h
+++ trunk/lukesw-version.h
@@ -1,7 +1,7 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 9
-#define VERSION_BUGFIX 3
+#define VERSION_BUGFIX 4
 
-#define STRINGVERSION "0.9.3"
+#define STRINGVERSION "0.9.4"
 
 // rc compiler needs an empty line after this one
